779ca47b3ca48a4425cf88a9bb9f0802e01488f4
Gio Rapacciuolo <giorapac@gmail.com>
2015-05-04 17:25:00 -0700

    Fix color issue in server.R

--- server.R
+++ server.R
@@ -8,14 +8,16 @@ shinyServer(
                 # Subset taxon_table according to checklist and slider selection 
                 selection_subset <- subset(taxon_table, taxon == input$show_taxon &
                                              year >= input$range[1] &
-                                             year <= input$range[2]) 
-            
+                                             year <= input$range[2])             
                 # Table up subset data 
                 selection_table <- table(selection_subset$taxon, selection_subset$year)
-                # Define colors based on checklist selection
+                # Keep nonzero rows
+                selection_table <- selection_table[rowSums(selection_table) != 0,]
+                # Define a set of six default colors 
                 default_colors <- c("#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#dd4477")
-                names(default_colors) <- as.character(unique(selection_subset$taxon))
-                plot_colors <- default_colors[row.names(selection_table) %in% input$show_taxon]
+                names(default_colors) <- as.character(unique(taxon_table$taxon))
+                # Define color subset based on checklist selection
+                plot_colors <- default_colors[row.names(selection_table)]
                 # Plot subset data
                 barplot(selection_table, 
                         col = plot_colors)
